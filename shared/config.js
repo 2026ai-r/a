87cde1107e5b4520fedd849a8eae2720454391d5
raonAI <raaoaon232@gmail.com>
2026-06-10 12:46:06 +0900

    Add files via upload

---
+++ shared/config.js
@@ -0,0 +1,6 @@
+// AI PROCESS Supabase project config
+// Project Settings > API 에서 Project URL / anon key 확인
+// URL에는 /rest/v1 을 붙이지 마세요.
+
+window.AI_PROCESS_SUPABASE_URL = 'https://viqbvqfibgpwtyvwzsbn.supabase.co';
+window.AI_PROCESS_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpcWJ2cWZpYmdwd3R5dnd6c2JuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4OTg0ODcsImV4cCI6MjA5NjQ3NDQ4N30.KOLbNTBRf9lrYeQaIMmD1uZEGeH184XJOi1V0b_xVXg';
